f15157b1d3cd05b6ba496a2da851fe1729cdd805
kbalubam <kevin.baluba@hotmail.com>
2021-07-11 18:22:02 +0200

    clavier num bug

--- public/js/calculs.js
+++ public/js/calculs.js
@@ -25,6 +25,143 @@ tabBtnNumber.forEach(element => {
         } 
     });
 });
+document.body.addEventListener("keydown", (e)=>{
+   if (e.keyCode=="48"){
+
+    if (input.getAttribute("check") == "ok") {
+        temp += 0;
+        
+        input.value += 0;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=0;
+        input.value += 0;
+        
+        
+    } 
+   } else if (e.keyCode=="49"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 1;
+        
+        input.value += 1;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=1;
+        input.value += 1;
+        
+    } 
+   
+   } else if (e.keyCode=="50"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 2;
+        
+        input.value += 2;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=2;
+        input.value += 2;
+        
+        
+    } 
+   } else if (e.keyCode=="51"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 3;
+        
+        input.value += 3;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=3;
+        input.value += 3;
+        
+        
+    } 
+   }
+   else if (e.keyCode=="52"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 4;
+        
+        input.value += 4;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=4;
+        input.value += 4;
+        
+        
+    } 
+   }else if (e.keyCode=="53"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 5;
+        
+        input.value += 5;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=5;
+        input.value += 5;
+        
+        
+    } 
+   }else if (e.keyCode=="54"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 6;
+        
+        input.value += 6;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=6;
+        input.value += 6;
+        
+        
+    } 
+   }else if (e.keyCode=="55"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 7;
+        
+        input.value += 7;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=7;
+        input.value += 7;
+        
+        
+    } 
+   }else if (e.keyCode=="56"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 8;
+        
+        input.value += 8;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=8;
+        input.value += 8;
+        
+        
+    } 
+   }else if (e.keyCode=="57"){
+    if (input.getAttribute("check") == "ok") {
+        temp += 9;
+        
+        input.value += 9;
+        
+    }
+    else if(input.getAttribute("check") == "not ok") {
+        temp2 +=9;
+        input.value += 9;
+    }
+}
+    
+        
+        
+    
+})
 
 
 let tabOperation = document.querySelector(".operation").querySelectorAll(".numberTaille2");
@@ -69,6 +206,7 @@ document.body.addEventListener("keydown", (e) => {
         input.value = Math.floor(parseInt(temp) / parseInt(temp2));
     }
 });
+
 
 
 
